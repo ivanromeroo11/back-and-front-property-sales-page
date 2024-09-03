2a26ce0d54a682ec249d80a32a477a88419a9daf
ivan <ivanromero5520@gmail.com>
2024-09-03 03:23:40 +0200

    init npm + require , import , routing and port

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hello, world!');
+// Requerir y definir en una variable con Comonjs
+
+//const express = require('express')
+
+// Importar con ESM 
+
+import express from 'express'
+
+
+// Crear la app para llamar a express 
+
+const app = express()
+
+// Routing
+
+app.get('/', function(req, res) {
+    
+    res.send('Hello world in express')
+      
+});
+
+
+// Definir puerto y arrancarlo
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Funciona en el puerto ${port}`)
+
+});
+
+
