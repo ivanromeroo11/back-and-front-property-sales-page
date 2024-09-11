7ff73ac7e4065b3ec6bc6033909ddb6dcb22684f
ivan <ivanromero5520@gmail.com>
2024-09-11 21:37:31 +0200

    validation in all fields and errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from "../models/Usuario.js"
+
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
@@ -14,7 +16,26 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    //Validación
+    await check('nombre').notEmpty().withMessage('El Nombre es obligatorio').run(req);
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req);
+    await check('password').isLength({min: 6 }).withMessage('Escribe al menos 6 caracteres en el password').run(req);
+    await check('repetir_password').equals('password').withMessage('Los password no coinciden').run(req);
+
+    let resultado = validationResult(req);
+
+    // Verificar que el resultado no este vacío
+
+    if(!resultado.isEmpty()) {
+        //Errores
+       return res.render('auth/registro', {
+            pagina: 'Crear cuenta',
+            errores: resultado.array()
+       })
+    }
    
+    
+
     const usuario = await Usuario.create(req.body)
 
     res.json(usuario)
