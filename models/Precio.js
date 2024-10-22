4ac5f4f310a7b2196291a810fb2f07ed58f31c0e
ivan <ivanromero5520@gmail.com>
2024-10-22 18:32:40 +0200

    property models, price and category

---
+++ models/Precio.js
@@ -0,0 +1,18 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db.js';
+
+const Precio = db.define('precios',{
+    precio:{
+        type: DataTypes.STRING(30),
+        allowNull: false
+    }
+
+});
+
+
+
+
+
+
+
+export default Precio ;
