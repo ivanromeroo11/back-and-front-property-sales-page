4df6f942db3421d0ac0ee699fc40b7b131f237b3
ivan <ivanromero5520@gmail.com>
2024-11-09 04:09:15 +0100

    adding a dropzone

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -10,13 +10,13 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/mapa.js":
-/*!*********************!*\
-  !*** ./src/mapa.js ***!
-  \*********************/
+/***/ "./src/js/mapa.js":
+/*!************************!*\
+  !*** ./src/js/mapa.js ***!
+  \************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n\n(function() {\n    // Logical OR\n    const lat =  document.querySelector('#lat').value || 36.9208667;\n    const lng = document.querySelector('#lng').value || -5.5388264;\n    const mapa = L.map('mapa').setView([lat, lng ], 16);\n    let marker;\n\n    //Utilizar Provider y Geocoder\n\n    const geocodeService = L.esri.Geocoding.geocodeService();\n    \n\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(mapa);\n\n    //el pin\n    marker = new L.marker([lat, lng],{\n        draggable: true,\n        autoPan: true\n    })\n    .addTo(mapa)\n\n    //Detectar el movimiento del pin \n\n    marker.on('moveend', function(e){\n        marker = e.target\n        const posicion = marker.getLatLng();\n        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));\n\n        // Obtener la información de la ubicación\n        geocodeService.reverse().latlng(posicion, 16).run(function(error, result) {\n            console.log(result);\n\n            marker.bindPopup(result.address.LongLabel);\n\n            // Llenar los campos de dirección\n\n            document.querySelector('.calle').textContent = result?.address?.Address ?? '';\n            document.querySelector('#calle').value = result?.address?.Address ?? '';\n            document.querySelector('#lat').value = result?.latlng?.lat ?? '';\n            document.querySelector('#lng').value = result?.latlng?.lng ?? '';\n        })\n           \n    })\n\n})()\n\n//# sourceURL=webpack://backend/./src/mapa.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n\n(function() {\n    // Logical OR\n    const lat =  document.querySelector('#lat').value || 36.9208667;\n    const lng = document.querySelector('#lng').value || -5.5388264;\n    const mapa = L.map('mapa').setView([lat, lng ], 16);\n    let marker;\n\n    //Utilizar Provider y Geocoder\n\n    const geocodeService = L.esri.Geocoding.geocodeService();\n    \n\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(mapa);\n\n    //el pin\n    marker = new L.marker([lat, lng],{\n        draggable: true,\n        autoPan: true\n    })\n    .addTo(mapa)\n\n    //Detectar el movimiento del pin \n\n    marker.on('moveend', function(e){\n        marker = e.target\n        const posicion = marker.getLatLng();\n        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));\n\n        // Obtener la información de la ubicación\n        geocodeService.reverse().latlng(posicion, 16).run(function(error, result) {\n            console.log(result);\n\n            marker.bindPopup(result.address.LongLabel);\n\n            // Llenar los campos de dirección\n\n            document.querySelector('.calle').textContent = result?.address?.Address ?? '';\n            document.querySelector('#calle').value = result?.address?.Address ?? '';\n            document.querySelector('#lat').value = result?.latlng?.lat ?? '';\n            document.querySelector('#lng').value = result?.latlng?.lng ?? '';\n        })\n           \n    })\n\n})()\n\n//# sourceURL=webpack://backend/./src/js/mapa.js?");
 
 /***/ })
 
@@ -43,7 +43,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n\n(function() {\n    // Logic
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/mapa.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	__webpack_modules__["./src/js/mapa.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
