efab14545538ab27f5a28818647b9f05211f4c04
ivan <ivanromero5520@gmail.com>
2024-11-29 18:21:46 +0100

    Rewriting the object in the database

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -258,6 +258,34 @@ const guardarCambios = async (req, res) => {
 
     // Reescribir el objeto y actualizarlo en la base de datos
 
+    try {
+        const { titulo, descripcion, habitaciones, aparcamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body;
+
+        propiedad.set({
+            titulo,
+            descripcion,
+            habitaciones,
+            aparcamiento,
+            wc,
+            calle,
+            lat,
+            lng,
+            precioId,
+            categoriaId
+        })
+
+        await propiedad.save();
+
+        res.redirect('/mis-propiedades');
+        
+
+
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+
 }
 
 
