d0b32521ff58a42a03b17141ff1658dbdcebee25
ivan <ivanromero5520@gmail.com>
2025-01-15 20:28:11 +0100

    validating numbers with a regular expression

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -5,7 +5,18 @@ import { Precio, Categoria, Propiedad } from '../models/index.js';
 
 
 const admin = async (req, res) => {
-    
+
+    //Leer queryString
+    const { pagina: paginaActual } = req.query;
+   
+    const expresion = /[0-9]/;
+
+    if(!expresion.test(paginaActual)){
+        return res.redirect('/mis-propiedades?pagina=1');
+    };
+
+
+
     const { id } = req.usuario;
 
     const propiedades = await Propiedad.findAll({
