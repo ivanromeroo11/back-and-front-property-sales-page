16a1315a5132a70a42d02d24b961ed368c08ddaa
ivan <ivanromero5520@gmail.com>
2025-01-20 18:46:19 +0100

    First steps to display a map

---
+++ src/js/mapaInicio.js
@@ -0,0 +1,12 @@
+(function(){
+    const lat = 36.9208667;
+    const lng = -5.5388264;
+    const mapa = L.map('mapa-inicio').setView([lat, lng ], 16);
+    let marker;
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+    
+})();
