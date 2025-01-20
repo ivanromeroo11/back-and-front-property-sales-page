97980df4333a9946405a813f2f6e4fa7a665af50
ivan <ivanromero5520@gmail.com>
2025-01-20 20:25:00 +0100

    Showing property pins

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -2,7 +2,8 @@
     const lat = 36.9208667;
     const lng = -5.5388264;
     const mapa = L.map('mapa-inicio').setView([lat, lng ], 16);
-    let marker;
+
+    let markers = new L.FeatureGroup().addTo(mapa);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,6 +14,8 @@
             const url = '/api/propiedades';
             const respuesta = await fetch(url);
             const propiedades = await respuesta.json();
+
+            mostrarPropiedades(propiedades);
            
             
         } catch (error) {
@@ -21,6 +24,22 @@
         }
 
     };
+
+    const mostrarPropiedades = propiedades  => {
+        propiedades.forEach(propiedad => {
+            //Agregar los pines 
+            const marker = new L.marker([propiedad?.lat, propiedad?.lng],{
+                autoPan: true
+            })
+            .addTo(mapa)
+            .bindPopup('INformación aquí')
+
+            markers.addLayer(marker);
+
+        })
+
+
+    }
 
     obtenerPropiedades();
 
