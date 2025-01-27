97045dd64cd21fbd69d718f70a311dad4ef426b0
ivan <ivanromero5520@gmail.com>
2025-01-27 15:09:31 +0100

    If the user is authenticated, they can send a message

--- middleware/identificarUsuario.js
+++ middleware/identificarUsuario.js
@@ -4,20 +4,28 @@ import Usuario from '../models/Usuario.js';
 const identificarUsuario = async (req, res, next) => {
 
     // Identificar si hay un token 
-    const token = req.cookies._token;
+    const {_token} = req.cookies;
 
-    if (!token) {
-        req.usuario = null;
+    if (!_token) {
+        req.usuario = null
         return next();
     }
     // Comprobar el token 
-    const { id } = jwt.verify(token, process.env.SECRETA);
+    try {
+        const decoded = jwt.verify(_token, process.env.JWT_SECRET);
+        const usuario = await Usuario.scope('eliminarPassword').findByPk(decoded.id);
 
-    const usuario = await Usuario.findByPk(id);
-
-    req.usuario = usuario;
-
-    next();
+        if(usuario) {
+            req.usuario = usuario;
+        }
+        return next();
+        
+    } catch (error) {
+        console.log(error);
+        return res.clearCookie('_token').redirect('/auth/login');
+        
+    }
+   
 };
 
 export default identificarUsuario;
