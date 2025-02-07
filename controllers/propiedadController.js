8acfe17e576c16656ecbd41e1d6a11b921dc70cd
ivan <ivanromero5520@gmail.com>
2025-02-07 20:15:16 +0100

    Checking who sent the message

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,6 +1,6 @@
 import { unlink } from 'node:fs/promises';
 import { validationResult } from 'express-validator';
-import { Precio, Categoria, Propiedad, Mensaje } from '../models/index.js';
+import { Precio, Categoria, Propiedad, Mensaje, Usuario} from '../models/index.js';
 import { esVendedor } from '../helpers/index.js';
 
 
@@ -456,8 +456,13 @@ const verMensajes = async (req, res) => {
     //Validar que la propiedad exista
     const propiedad = await Propiedad.findByPk(id,{
         include: [
-            { model: Mensaje, as: 'mensajes'}
+            { model: Mensaje, as: 'mensajes',
+                include: [
+                    { model: Usuario.scope('eliminarPassword'), as: 'usuario'}
+                ]
+            }
         ],
+        
     });
 
     if(!propiedad) {
