5cf8544b2e095d0d60754e6a751b0650bc0c5809
ivan <ivanromero5520@gmail.com>
2025-02-09 19:30:44 +0100

    Add csrftoken to the request

--- src/js/cambiarEstado.js
+++ src/js/cambiarEstado.js
@@ -1,18 +1,24 @@
 (function(){
     const cambiarEstadoBotones = document.querySelectorAll('.cambiar-estado');
-    cambiarEstadoBotones.forEach(boton => {
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    cambiarEstadoBotones.forEach( boton => {
         boton.addEventListener('click', cambiarEstadoPropiedad)
     });
 
     async function cambiarEstadoPropiedad(e) {
-        const { propiedadId: id } = e.target.dataset
+
+       const { propiedadId: id } = e.target.dataset
 
        try {
 
         const url = `/propiedades/${id}`
 
         const respuesta = await fetch(url, {
-            method: 'PUT'
+            method: 'PUT',
+            headers: { 
+                'CSRF-Token': token
+            }
         })
 
         console.log(respuesta)
