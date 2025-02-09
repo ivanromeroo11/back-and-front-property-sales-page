6f988d1bd6db6f84d4f0caeaba7d42b648d70a16
ivan <ivanromero5520@gmail.com>
2025-02-09 20:00:20 +0100

    Changing the state of the property in the html

--- src/js/cambiarEstado.js
+++ src/js/cambiarEstado.js
@@ -21,7 +21,22 @@
             }
         })
 
-        const resultado = await respuesta.json()
+        const { resultado } = await respuesta.json()
+
+        if(resultado) {
+            if(e.target.classList.contains('bg-yellow-100')) {
+                e.target.classList.add('bg-green-100', 'text-green-800')
+                e.target.classList.remove('bg-yellow-100', 'text-yellow-800')
+                e.target.textContent = 'Publicado'
+
+            }else{
+                e.target.classList.add('bg-yellow-100', 'text-yellow-800')
+                e.target.classList.remove('bg-green-100', 'text-green-800')
+                e.target.textContent = 'No Publicado'
+
+            }
+
+        }
         
        } catch (error) {
               console.log(error)
